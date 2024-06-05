a63164e589f6728474d95517d671b7de545cd87d
kanmur <kandu069@gmail.com>
2024-06-05 14:03:56 +0530

    new learn.sh file added

---
+++ learn.sh
@@ -0,0 +1,15 @@
+We are going to learn Bash shell.
+
+Here are the topics of the shell.
+
+Comments
+Print
+Variables
+Functions
+Conditions
+Loops
+Quotes
+Exit Status
+Inputs
+SED Editor -> Parking for later time
+We are going to learn BASH SHELL
